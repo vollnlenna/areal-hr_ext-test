3e6dd34a1e4ca845c4c385b61b1bb7f40923b483
elena <vollnlenna57@gmail.com>
2025-11-23 02:05:57 +0300

    add migration for positions table

---
+++ api/migrations/1763852429986_positions.js
@@ -0,0 +1,38 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ */
+export const up = (pgm) => {
+  pgm.createTable('positions', {
+    id_position: {
+      type: 'serial',
+      primaryKey: true
+    },
+    name: {
+      type: 'varchar(255)',
+      notNull: true
+    },
+    created_at: {
+      type: 'timestamp',
+      notNull: true,
+      default: pgm.func('now()')
+    },
+    updated_at: {
+      type: 'timestamp'
+    },
+    deleted_at: {
+      type: 'timestamp'
+    }
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ */
+export const down = (pgm) => {
+  pgm.dropTable('positions');
+};
