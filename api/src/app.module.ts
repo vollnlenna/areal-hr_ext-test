6df6c1df3a6920631d926dec872fd876aa548915
elena <vollnlenna57@gmail.com>
2025-11-23 21:09:00 +0300

    connect all modules to AppModule

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { OrganizationsModule } from './organizations/organizations.module';
+import { DepartmentsModule } from './departments/departments.module';
+import { PositionsModule } from './positions/positions.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    DatabaseModule,
+    OrganizationsModule,
+    DepartmentsModule,
+    PositionsModule,
+  ],
 })
 export class AppModule {}
