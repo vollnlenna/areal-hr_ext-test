0c4955494f3bc0649709c4a9c33999515849829e
elena <vollnlenna57@gmail.com>
2025-11-27 00:44:42 +0300

    update validation.ts - limit name to 255 chars

--- api/src/validation.ts
+++ api/src/validation.ts
@@ -1,10 +1,12 @@
 import Joi from 'joi';
 
 export const validateOrganization = Joi.object({
-  name: Joi.string().min(2).required().messages({
+  name: Joi.string().min(2).max(255).required().messages({
     'string.empty': '‼️ Название организации обязательно',
     'string.base': '‼️ Название должно быть строкой',
     'string.min': '‼️ Название организации должно содержать минимум 2 символа',
+    'string.max':
+      '‼️ Название организации должно содержать не более 255 символов',
     'any.required': '‼️ Название обязательно',
   }),
 
@@ -14,9 +16,10 @@ export const validateOrganization = Joi.object({
 });
 
 export const validateDepartment = Joi.object({
-  name: Joi.string().min(2).required().messages({
+  name: Joi.string().min(2).max(255).required().messages({
     'string.empty': '‼️ Название отдела обязательно',
     'string.min': '‼️ Название отдела должно содержать минимум 2 символа',
+    'string.max': '‼️ Название отдела должно содержать не более 255 символов',
     'any.required': '‼️ Название обязательно',
   }),
 
@@ -33,9 +36,11 @@ export const validateDepartment = Joi.object({
 });
 
 export const validatePosition = Joi.object({
-  name: Joi.string().min(2).required().messages({
+  name: Joi.string().min(2).max(255).required().messages({
     'string.empty': '‼️ Название должности обязательно',
     'string.min': '‼️ Название должности должно содержать минимум 2 символа',
+    'string.max':
+      '‼️ Название должности должно содержать не более 255 символов',
     'any.required': '‼️ Название обязательно',
   }),
 });
