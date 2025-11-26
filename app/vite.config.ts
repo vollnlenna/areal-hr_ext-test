b73ac95d85d1aba2c02e63b45cc9a359e54e693b
elena <vollnlenna57@gmail.com>
2025-11-26 13:49:45 +0300

    correct vite config syntax and add env support

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,18 +1,35 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd() + '/..')
+
+  return {
+    envDir: '..',
+
+    plugins: [
+      vue(),
+      vueDevTools(),
+    ],
+
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
     },
-  },
+
+    server: {
+      port: Number(env.VITE_APP_PORT),
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        }
+      }
+    }
+  }
 })
