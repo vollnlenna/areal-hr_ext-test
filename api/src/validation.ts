a7c6439927d9abd421227a50d331f9244b19e02c
elena <vollnlenna57@gmail.com>
2025-11-26 17:14:49 +0300

    update validation messages

--- api/src/validation.ts
+++ api/src/validation.ts
@@ -2,39 +2,39 @@ import Joi from 'joi';
 
 export const validateOrganization = Joi.object({
   name: Joi.string().min(2).required().messages({
-    'string.empty': 'Название организации обязательно',
-    'string.base': 'Название должно быть строкой',
-    'string.min': 'Название организации должно содержать минимум 2 символа',
-    'any.required': 'Название обязательно',
+    'string.empty': '‼️ Название организации обязательно',
+    'string.base': '‼️ Название должно быть строкой',
+    'string.min': '‼️ Название организации должно содержать минимум 2 символа',
+    'any.required': '‼️ Название обязательно',
   }),
 
   comment: Joi.string().allow(null).optional().messages({
-    'string.base': 'Комментарий должен быть строкой',
+    'string.base': '‼️ Комментарий должен быть строкой',
   }),
 });
 
 export const validateDepartment = Joi.object({
   name: Joi.string().min(2).required().messages({
-    'string.empty': 'Название отдела обязательно',
-    'string.min': 'Название отдела должно содержать минимум 2 символа',
-    'any.required': 'Название обязательно',
+    'string.empty': '‼️ Название отдела обязательно',
+    'string.min': '‼️ Название отдела должно содержать минимум 2 символа',
+    'any.required': '‼️ Название обязательно',
   }),
 
   id_organization: Joi.number().required().messages({
-    'any.required': 'Не выбрана организация',
+    'any.required': '‼️ Не выбрана организация',
   }),
 
   id_parent_department: Joi.number().allow(null).optional(),
 
   comment: Joi.string().allow(null).optional().messages({
-    'string.base': 'Комментарий должен быть строкой',
+    'string.base': '‼️ Комментарий должен быть строкой',
   }),
 });
 
 export const validatePosition = Joi.object({
   name: Joi.string().min(2).required().messages({
-    'string.empty': 'Название должности обязательно',
-    'string.min': 'Название должности должно содержать минимум 2 символа',
-    'any.required': 'Название обязательно',
+    'string.empty': '‼️ Название должности обязательно',
+    'string.min': '‼️ Название должности должно содержать минимум 2 символа',
+    'any.required': '‼️ Название обязательно',
   }),
 });
